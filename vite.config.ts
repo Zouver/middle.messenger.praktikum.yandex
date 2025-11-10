79b6f3bccd005f0d8c6c603b4a66cb6006c59ea5
Andrey Ardvenev <a.ardvenev@bi.zone>
2025-11-10 19:16:04 +0300

    Rollup settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,49 @@ import handlebars from 'vite-plugin-handlebars';
 
 const pageData: Record<string, object> = {
   '/index.html': {
-    title: 'Main Page',
+    title: 'Main',
+  },
+  '/login.html': {
+    title: 'Login',
+  },
+  '/signup.html': {
+    title: 'Signup',
+  },
+  '/error-404.html': {
+    title: 'Error',
+    errorCode: "404",
+    errorContent: "Не туда попали"
+  },
+  '/error-500.html': {
+    title: 'Error',
+    errorCode: "500",
+    errorContent: "Мы уже фиксим"
   }
 };
 
+function getInput() {
+  const input: Record<string, string> = {}
+
+  Object.keys(pageData).forEach((key, index) => {
+    input[index.toString()] = resolve(srcDir, key)
+  })
+
+  return input
+}
+
 const srcDir = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'dist');
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: resolve(srcDir, './pages'),
-  build: {outDir},
+  root: srcDir,
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    copyPublicDir: true,
+    rollupOptions: {
+      input: getInput(),
+    }},
   plugins: [handlebars({
     context: (pagePath: string) => pageData[pagePath],
-    partialDirectory: resolve(srcDir,'./partials'),
+    partialDirectory: resolve(srcDir,'./components'),
   })],
 })

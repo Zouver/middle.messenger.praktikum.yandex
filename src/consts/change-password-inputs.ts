e605d114bb6dd0f99f6d0109f9587337814d320d
Andrey Ardvenev <a.ardvenev@bi.zone>
2025-11-17 22:13:31 +0300

    Change password inputs

---
+++ src/consts/change-password-inputs.ts
@@ -0,0 +1,20 @@
+import type {IKeyValueItem} from "./types.ts";
+
+
+export const changePasswordInputs: IKeyValueItem[] = [
+	{
+		key: "Старый пароль",
+		value: "pochta@yandex.ru",
+		name: "oldPassword"
+	},
+	{
+		key: "Новый пароль",
+		value: "ivanivanov",
+		name: "newPassword"
+	},
+	{
+		key: "Повторите новый пароль",
+		value: "ivanivanov",
+		name: "newPasswordSubmit"
+	},
+];

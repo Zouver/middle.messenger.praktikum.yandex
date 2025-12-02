caed73663e218d8eba0fb8a32a14c97a94fc40b9
Andrey Ardvenev <a.ardvenev@bi.zone>
2025-12-02 15:26:50 +0300

    API

---
+++ src/components/widgets/index.ts
@@ -0,0 +1,13 @@
+
+export {Chat} from "./chat";
+export {ChatList} from './chat-list'
+export {ChatPreview} from "./chat-preview";
+export {Login} from "./login";
+export {Navigation} from "./navigation";
+export {Signup} from "./signup";
+export {Message} from "./message";
+export {ProfileNav} from './profile-nav'
+export {ProfilePicture} from "./profile-picture";
+export {ProfileInformation} from "./profile-information";
+export {ProfileChangePassword} from "./profile-change-password";
+export {ProfileActions} from "./profile-actions";

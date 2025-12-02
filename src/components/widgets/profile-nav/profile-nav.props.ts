3929ac8a280f665b7cd3942a6f73f508e2a508fd
Andrey Ardvenev <a.ardvenev@bi.zone>
2025-12-02 15:26:30 +0300

    profile-nav

---
+++ src/components/widgets/profile-nav/profile-nav.props.ts
@@ -0,0 +1,7 @@
+import {type BaseProps} from '@/lib'
+import {ButtonIcon} from "@components/shared";
+
+export interface ProfileNavProps extends BaseProps{
+	button?: ButtonIcon;
+}
+
---
+++ src/components/widgets/profile-nav/profile-nav.ts
@@ -0,0 +1,27 @@
+import {Component} from "@/lib";
+import {ButtonIcon} from "@components/shared";
+
+import {profileNavTemplate} from "./profile-nav.template.ts";
+
+import type {ProfileNavProps} from "./profile-nav.props.ts";
+import './profile-nav.css'
+
+export class ProfileNav extends Component<ProfileNavProps> {
+	constructor(props: ProfileNavProps = {}) {
+		const button = props.button || new ButtonIcon({
+			variant: "primary",
+			icon: "fa-solid fa-arrow-left"
+		});
+
+		super(
+			"nav",
+			{...props, button},
+			["profile-navigation"]
+		);
+	}
+
+	render() {
+		return this.compile(profileNavTemplate, this.props);
+	}
+}
+

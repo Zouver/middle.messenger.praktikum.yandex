a8d0292277e390eda125074088a6ca8d580bac95
Andrey Ardvenev <a.ardvenev@bi.zone>
2025-12-02 20:03:05 +0300

    Router, routes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import "./styles/tokens.css"
-import "./styles/styles.css"
-import "./styles/typography.css"
-import "./styles/components.scss"
+import "./styles/tokens.css";
+import "./styles/styles.css";
+import {Router} from "@/lib";
+import {errorRoutes, routes} from "@/routes.ts";
+
+const router = new Router(routes, errorRoutes);
+router.ready();

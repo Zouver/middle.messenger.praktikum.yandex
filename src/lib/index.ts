d02163a450a6c18eea0c100c7017fe5380495d4b
Andrey Ardvenev <a.ardvenev@bi.zone>
2025-12-02 20:02:52 +0300

    API

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
-export {EventBus} from './event-bus'
-export {Component} from './component'
-export type {BaseProps} from './component/types.ts'
-export {render} from './render'
+export {EventBus} from './event-bus';
+export {Router} from './router';
+export {Component} from './component';
+export type {BaseProps} from './component/types.ts';
+export {render} from './render';

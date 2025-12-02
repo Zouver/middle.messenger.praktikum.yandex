c96f4653eed6b0909c7c268008ead65f345cab97
Andrey Ardvenev <a.ardvenev@bi.zone>
2025-12-02 15:27:24 +0300

    key-value-list

---
+++ src/components/shared/key-value-list/key-value-list.props.ts
@@ -0,0 +1,8 @@
+import {type BaseProps} from '@/lib'
+import {KeyValueItem} from "@components/shared";
+
+
+export interface KeyValueListProps extends BaseProps {
+	items: KeyValueItem[];
+}
+
---
+++ src/components/shared/key-value-list/key-value-list.ts
@@ -0,0 +1,22 @@
+import {Component} from "@/lib";
+
+import {keyValueListTemplate} from "./key-value-list.template.ts";
+
+import type { KeyValueListProps} from "./key-value-list.props.ts";
+import './key-value-list.css'
+
+export class KeyValueList extends Component<KeyValueListProps> {
+
+	constructor(props: KeyValueListProps) {
+		super(
+			"div",
+			props,
+			["key-value-list"]
+		);
+	}
+
+	render() {
+		return this.compile(keyValueListTemplate, this.props);
+	}
+}
+

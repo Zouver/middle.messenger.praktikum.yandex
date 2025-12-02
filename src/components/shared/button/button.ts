cd41201f3918036ad705e2dd6b3ac917ab1c3ffe
Andrey Ardvenev <a.ardvenev@bi.zone>
2025-12-02 15:26:55 +0300

    button

---
+++ src/components/shared/button/button.ts
@@ -0,0 +1,26 @@
+import {Component} from "@/lib";
+import {buttonTemplate} from "@components/shared/button/button.template.ts";
+
+import type {ButtonProps} from "@components/shared/button/button.props.ts";
+import './button.css'
+
+export class Button extends Component<ButtonProps> {
+	constructor(props: ButtonProps) {
+
+		const classNames = ["button"];
+		if(props.variant) classNames.push(`button-${props.variant}`);
+
+
+		super(
+			"button",
+			{...props},
+			classNames,
+			{type: props.type || "button"},
+		);
+
+	}
+
+	render() {
+		return this.compile(buttonTemplate, this.props);
+	}
+}

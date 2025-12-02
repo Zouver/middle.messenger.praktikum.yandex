32663a12b42c536288205b2a837796e05a895b8e
Andrey Ardvenev <a.ardvenev@bi.zone>
2025-12-02 15:23:38 +0300

    Eslint import plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,44 @@ import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import importPlugin from 'eslint-plugin-import';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['src/**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      importPlugin.flatConfigs.recommended,
     ],
+    rules: {
+      "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+          pathGroupsExcludedImportTypes: [],
+        },
+      ],
+    },
+    settings:{
+      "import/resolver": {
+        "typescript": true,
+      },
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,

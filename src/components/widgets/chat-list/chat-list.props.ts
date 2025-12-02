4d1c40f0f10bc32e5950b5508f97380fc71af031
Andrey Ardvenev <a.ardvenev@bi.zone>
2025-12-02 15:25:49 +0300

    chat-list

---
+++ src/components/widgets/chat-list/chat-list.props.ts
@@ -0,0 +1,10 @@
+import {type BaseProps} from '@/lib'
+import {Input, Text} from "@components/shared";
+import {ChatPreview} from "@components/widgets";
+
+export interface ChatListProps extends BaseProps{
+	text?: Text;
+	search?: Input;
+	chats: ChatPreview[];
+}
+
---
+++ src/components/widgets/chat-list/chat-list.ts
@@ -0,0 +1,37 @@
+import {Component} from "@/lib";
+import {Input, Text} from "@components/shared"
+
+import {chatListTemplate} from "./chat-list.template.ts";
+
+import type {ChatListProps} from "./chat-list.props.ts";
+import './chat-list.css'
+
+export class ChatList extends Component<ChatListProps> {
+	constructor(props: ChatListProps) {
+		const text = props.text || new Text({
+			text: "Профиль >",
+			variant: "secondary"
+		});
+
+		const search = props.search || new Input({
+			type: "text",
+			placeholder: "Поиск",
+			name:  "search",
+		});
+
+		super(
+			"section",
+			{...props, text, search},
+			[],
+			{id: "chat-list"}
+		);
+	}
+
+	render() {
+		return this.compile(
+			chatListTemplate,
+			this.props
+		);
+	}
+}
+

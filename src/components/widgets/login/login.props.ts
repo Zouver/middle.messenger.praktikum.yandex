530c4ae6fab0c0ad98ae6855e375ce9baa708bd9
Andrey Ardvenev <a.ardvenev@bi.zone>
2025-12-04 00:37:37 +0300

    Login

--- src/components/widgets/login/login.props.ts
+++ src/components/widgets/login/login.props.ts
@@ -1,9 +1,11 @@
 import {type BaseProps} from '@/lib/component';
-import {TextHeading, Input, Button} from "@components/shared";
+import {Input, Button, TextHeading} from "@components/shared";
+import {AuthForm} from "@components/widgets";
 
 
 export interface LoginProps extends BaseProps{
 	heading?: TextHeading;
+	authForm?: AuthForm;
 	inputs?: Input[];
 	buttons?: Button[];
 }
--- src/components/widgets/login/login.template.ts
+++ src/components/widgets/login/login.template.ts
@@ -1,17 +1,6 @@
 // language=hbs
 export const loginTemplate = `
   {{{ heading }}}
-  <form class="auth-form">
-    <div class="form-group">
-      {{#each inputs}}
-        {{{ this }}}
-      {{/each}}
-    </div>
-    <div class="button-group">
-      {{#each buttons}}
-        {{{ this }}}
-      {{/each}}
-    </div>
-  </form>
+  {{{ authForm }}}
 `;
 

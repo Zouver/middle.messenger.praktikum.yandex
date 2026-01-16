4a4d9018d239af1c984282298915693c13356bbf
Andrey Ardvenev <a.ardvenev@bi.zone>
2026-01-16 15:54:22 +0300

    ChatList

--- src/components/widgets/chat-list/chat-list.props.ts
+++ src/components/widgets/chat-list/chat-list.props.ts
@@ -1,11 +1,15 @@
 import {type BaseProps} from '@/lib/component';
 import {Input, Text} from "@components/shared";
-import {ChatPreview} from "@components/widgets";
+import {ChatPreview, CreateChat} from "@components/widgets";
 
+import type {ChatType} from "@/api/type.ts";
 
 export interface ChatListProps extends BaseProps{
 	text?: Text;
 	search?: Input;
 	chats: ChatPreview[];
+	createChat?: CreateChat;
+	onCreateChat: (chat: Pick<ChatType, "id">) => void;
+	onSearch: (value: string) => void;
 }
 
--- src/components/widgets/chat-list/chat-list.ts
+++ src/components/widgets/chat-list/chat-list.ts
@@ -1,32 +1,47 @@
 import {Component} from "@/lib/component";
 import {Input, Text} from "@components/shared";
+import {CreateChat} from "@components/widgets";
 
 import {chatListTemplate} from "./chat-list.template.ts";
 
 import type {ChatListProps} from "./chat-list.props.ts";
+import type {Events} from "@lib/component/types.ts";
 
 import './chat-list.css';
 
 export class ChatList extends Component<ChatListProps> {
 	constructor(props: ChatListProps) {
-		const text = props.text || new Text({
-			text: "Профиль >",
-			variant: "secondary"
+		super("section",props,[],{id: "chat-list"});
+	}
+
+	componentDidMount() {
+		const createChat = new CreateChat({
+			onCreateChat: this.props.onCreateChat.bind(this)
 		});
 
-		const search = props.search || new Input({
+		const text = new Text({
+			text: "Профиль >",
+			variant: "secondary",
+		});
+
+		const events: Events= {
+			change: (event: Event) => {
+				const input = event.target as HTMLInputElement;
+				const value = input.value;
+				this.props.onSearch(value);
+			}
+		};
+
+		const search = new Input({
 			type: "text",
 			placeholder: "Поиск",
 			name:  "search",
+			events: events
 		});
 
-		super(
-			"section",
-			{...props, text, search},
-			[],
-			{id: "chat-list"}
-		);
+		this.updateProps({ search, text, createChat });
 	}
+
 
 	render() {
 		return this.compile(

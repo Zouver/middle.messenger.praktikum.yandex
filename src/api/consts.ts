018beb3d1c7746590635dcad2be9f6a8edd9b70b
Andrey Ardvenev <a.ardvenev@bi.zone>
2026-01-19 15:01:33 +0300

    Resource url

--- src/api/consts.ts
+++ src/api/consts.ts
@@ -3,3 +3,4 @@ export const CHAT_ENDPOINT = '/api/v2/chats';
 export const AUTH_ENDPOINT = '/api/v2/auth';
 export const USER_ENDPOINT = '/api/v2/user';
 export const WS_URL = `wss://ya-praktikum.tech/ws`;
+export const RESOURCES_URL = `${API_URL}/api/v2/resources`;
